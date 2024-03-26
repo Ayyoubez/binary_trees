45555ab38185d56a10d343160610ccd17196624b
ayyoubez <ayyoubez@gmail.com>
2024-03-26 16:26:59 +0000

    next

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,19 +11,19 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-        if (parent == NULL)
-                return (NULL);
-        new_node = binary_tree_node(parent, value);
-        if (new_node == NULL)
-                return (NULL);
-        if (parent->right != NULL)
-        {
-                new_node->right = parent->right;
-                parent->right->parent = new_node;
-        }
+	if (parent == NULL)
+		return (NULL);
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
 	parent->right = new_node;
-        new_node->parent = parent;
-        return (new_node);
+	new_node->parent = parent;
+	return (new_node);
 }
