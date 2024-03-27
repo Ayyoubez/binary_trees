f7f4814b459149c05d31b8daf49c197759827d7a
ayyoubez <ayyoubez@gmail.com>
2024-03-27 01:31:14 +0000

    Next

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,11 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	else
-	{
-		left_height = tree->left ? 1 + (binary_tree_height(tree->left)) : 0;
-		right_height = tree->right ? 1 + (binary_tree_height(tree->right)) : 0;
-	}
+	left_height = tree->left ? 1 + (binary_tree_height(tree->left)) : 0;
+	right_height = tree->right ? 1 + (binary_tree_height(tree->right)) : 0;
 	return ((left_height > right_height) ? left_height : right_height);
 
 }
